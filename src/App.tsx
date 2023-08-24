f35204dbad72b2a95ae8455fe0f36e6995cf1caf
tanase-j-ww <tanase.j@world-wing.com>
2023-08-24 16:11:21 +0900

    test:ファーストコミット

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChangeEvent, useState, FC } from "react";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export const App: FC = () => {
+  const [text, setText] =useState<string>("");
+  const [memos, setMemos] = useState<string[]>([]);
+
+  const onChangeText = (e: ChangeEvent<HTMLInputElement>) => setText(e.target.value);
+  
+  const onClickAdd = () => {
+    const newMemos = [...memos];
+    newMemos.push(text);
+    setMemos(newMemos);
+    setText("");
+  };
+
+  const onClickDelete = (index: number) => {
+    const newMemos = [...memos];
+    newMemos.splice(index, 1);
+    setMemos(newMemos);
+  };
+  
+  return(
+    <div>
+      <h1>メモアプリ</h1>
+      <input type="text" value={text} onChange={onChangeText} />
+      <SButton onClick={onClickAdd}>add</SButton>
+      <SContainer>
+        <p>memo list</p>
+        <ul>
+          {memos.map((memo, index) => (
+            <li key={memo}>
+              <SMemoWrapper>
+                <p>{memo}</p>
+                <SButton onClick={() => onClickDelete(index)}>delete</SButton>
+              </SMemoWrapper>
+            </li>
+          ))}
+        </ul>
+      </SContainer>
     </div>
-  );
-}
+  )
+};
 
-export default App;
+const SButton = styled.button`margin-left: 16px;`
+const SContainer = styled.div`border: solid 1px #ccc`
+const SMemoWrapper = styled.div`
+display: flex;
+align-items: center;
+`;
